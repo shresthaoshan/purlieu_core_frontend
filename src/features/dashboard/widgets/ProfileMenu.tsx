5b94f9d46623f713089de4d5671bdf52ae71fcdb
Oshan Shrestha <os02411418@student.ku.edu.np>
2022-04-16 00:01:01 +0545

    profile menu option set test

--- src/features/dashboard/widgets/ProfileMenu.tsx
+++ src/features/dashboard/widgets/ProfileMenu.tsx
@@ -1,12 +1,17 @@
-import React, { MouseEventHandler } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { useAppDispatch } from "../../../store";
 import { authActions } from "../../../store/slices/auth.slice";
 import useAuth from "../../auth/hooks/useAuth";
 
+import { m } from "framer-motion";
+
 const ProfileMenu = () => {
+	const [showMenu, setShowMenu] = useState<boolean>(false);
 	const { email } = useAuth();
 
 	const dispatch = useAppDispatch();
+
+	const updateMenuVisibility = () => setShowMenu(!showMenu);
 
 	const onLogout: MouseEventHandler = (e) => {
 		console.log({ msg: "Clicked..." });
@@ -17,14 +22,16 @@ const ProfileMenu = () => {
 
 	return (
 		<>
-			<div className="control__profile">
+			<div className="control__profile" onClick={updateMenuVisibility}>
 				<img src={`https://avatars.dicebear.com/api/miniavs/${email}.svg`} alt={email} />
-				<div className="control__profile-menu">
-					<ul>
-						<li className="label">Actions</li>
-						<li onClick={onLogout}>Logout</li>
-					</ul>
-				</div>
+				{showMenu ? (
+					<m.div className="control__profile-menu" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+						<ul>
+							<li className="label">Actions</li>
+							<li onClick={onLogout}>Logout</li>
+						</ul>
+					</m.div>
+				) : null}
 			</div>
 		</>
 	);
